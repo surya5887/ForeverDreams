e08f463fc822cb57892aa2d088d19563922e6246
unlockpro8558 <unlockpro8558@gmail.com>
2026-07-10 09:39:46 +0530

    Fix navbar and footer logo sizes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -34,7 +34,7 @@ export default function Footer() {
             <div className={styles.fLogoCircle}>
               <img src="/main_logo.jpeg" alt="FD Logo" className={styles.fLogoIconImage} />
             </div>
-            <img src="/name_img.png" alt="Forever Dreams Home" className={styles.nameImgFooter} style={{ height: '40px', width: 'auto', objectFit: 'contain', filter: 'brightness(0) invert(1)' }} />
+            <img src="/name_img.png" alt="Forever Dreams Home" className={styles.nameImgFooter} style={{ height: '40px', width: 'auto', objectFit: 'contain', filter: 'brightness(0) invert(1)', transform: 'scale(2.2)', transformOrigin: 'left center', marginLeft: '15px' }} />
           </div>
           <p className={styles.footerDesc}>
             Designing beautiful spaces that reflect your style and personality.
